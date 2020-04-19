65beea7c670875fee1c18590f9f84b6edf1d7457
txiflis <txiflis@hotmail.com>
2020-04-19 14:34:00 +0200

    Sisira 10

--- config/application_custom.rb
+++ config/application_custom.rb
@@ -9,12 +9,12 @@ module Consul
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
-    config.i18n.default_locale = :eu
-    config.i18n.available_locales = [:en, :es, :eu]
+    config.i18n.default_locale = 'eu-ES'
+    config.i18n.available_locales = [:en, :es, 'eu-ES']
     #config.i18n.default_locale = :en
     #config.i18n.available_locales = [:en, :es, :fr, :nl, 'pt-BR']
 
-    config.i18n.fallbacks = {'eu' => 'es'}
+    config.i18n.fallbacks = {'eu-ES' => 'es'}
 
    
   end
