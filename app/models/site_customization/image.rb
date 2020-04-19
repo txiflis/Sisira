2f46910779ff1c8e4d870a8dcbd53f4acf4b06e3
txiflis <txiflis@hotmail.com>
2020-04-19 13:00:18 +0200

    Sisira 09

--- app/models/site_customization/image.rb
+++ app/models/site_customization/image.rb
@@ -1,6 +1,6 @@
 class SiteCustomization::Image < ApplicationRecord
   VALID_IMAGES = {
-    "logo_header" => [260, 80],
+    "logo_header" => [210, 80],
     "social_media_icon" => [470, 246],
     "social_media_icon_twitter" => [246, 246],
     "apple-touch-icon-200" => [200, 200],
